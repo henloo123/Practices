7cfcc9f6145ceebb7960aecce40a42958dfd7a3d
henloo123 <shua0257@gmail.com>
2024-12-01 13:27:16 +0800

    Update pe4.java

--- Java OOP/Finals Programming Exercises/pe4.java
+++ Java OOP/Finals Programming Exercises/pe4.java
@@ -11,16 +11,107 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class pe4 extends JFrame
+public class pe4 extends JFrame implements ItemListener
 {
    final int PARKSIDE = 600;
    final int POOLSIDE = 750;
    final int LAKESIDE = 825;
+   final int BED = 75;
+   final int MEAL = 200;
    int total = 0;
+   
+   JLabel title = new JLabel("Lambert's Vacation Rentals");
+   JLabel loc = new JLabel("Select Location");
+   JLabel bed = new JLabel("Select how many bed/s");
+   JLabel tot = new JLabel("Total Cost ");
+   
+   JTextField calc = new JTextField("$" + total, 4);
+   
+   JCheckBox loc1 = new JCheckBox("Parkside $" + PARKSIDE);
+   JCheckBox loc2 = new JCheckBox("Poolside $" + POOLSIDE);
+   JCheckBox loc3 = new JCheckBox("Lakeside $" + LAKESIDE);
+   
+   JCheckBox bed1 = new JCheckBox("One");
+   JCheckBox bed2 = new JCheckBox("Two");
+   JCheckBox bed3 = new JCheckBox("Three");
+   
+   JCheckBox meal = new JCheckBox("With Meal");
+   
+   ButtonGroup locs = new ButtonGroup();
+   ButtonGroup beds = new ButtonGroup();
    
    public pe4()
    {
+      super("Lambert's Vacation Rentals");
+      setSize(500, 350);
+      setLayout(new FlowLayout());
+      setVisible(true);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+      title.setFont(new Font("Times New Roman", Font.BOLD, 35));
+      
+      
+      locs.add(loc1);
+      locs.add(loc2);
+      locs.add(loc3);
+      
+      
+      beds.add(bed1);
+      beds.add(bed2);
+      beds.add(bed3);
+      
+      add(title);
+      add(Box.createVerticalStrut(80));
+      
+      add(loc);
+      add(Box.createHorizontalStrut(45));
+      add(loc1);
+      add(loc2);
+      add(loc3);
+      
+      add(bed);
+      add(Box.createHorizontalStrut(90));
+      add(bed1);
+      add(bed2);
+      add(bed3);
+      add(Box.createHorizontalStrut(60));
+      
+      add(meal);
+      
+      add(tot);
+      add(calc);
+   }
    
+   @Override
+   public void itemStateChanged(ItemEvent e)
+   {
+      Object source = e.getSource();
+      int selected = e.getStateChange();
+      if(source == loc1)
+      {
+         if(selected == ItemEvent.SELECTED)
+            total += PARKSIDE;
+         else
+            total -= PARKSIDE;
+      }
+      else if (source == loc2)
+      {
+         if(selected == ItemEvent.SELECTED)
+            total += POOLSIDE;
+         else
+            total -= POOLSIDE;
+      }
+      else
+      {
+         if(selected == ItemEvent.SELECTED)
+            total += LAKESIDE;
+         else
+            total -= LAKESIDE;
+      }
+      
+      calc.setText("$" + total);
+      revalidate();
+      repaint();
    }
 }
 
